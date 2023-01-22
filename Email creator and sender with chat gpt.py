e93169ddb87f2dcc9863e6b841080512d86fec45
Paolo Caneparo <66754232+leonida300-gg@users.noreply.github.com>
2023-01-22 11:21:45 +0100

    Update Email creator and sender with chat gpt.py

--- Email creator and sender with chat gpt.py
+++ Email creator and sender with chat gpt.py
@@ -8,55 +8,54 @@ import time
 import random
 
 # Set the OpenAI API key
-openai.api_key = "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"
-
-#import the email, name investor and company website frome excel 
-import pandas as pd
-
-path = "C:/Users/caneparo/Desktop/Paolo's code projects/trial excel python code.xlsx"
-df = pd.read_excel(path)
-data_vc_final = df[['Website (if available)', 'Partner Name', 'Partner Email']].dropna(axis= 0)
+openai.api_key = "add your Open Ai key"
 
 #server definition
 server = None
 
-sender_email = "paolo@wzl.io"
-sender_password = "PaoloWZL0601"
-pdf_file = "C:\\Users\\caneparo\\Desktop\\CV_Paolo_Caneparo.pdf"
+investor_name_list = ["Kuber Bansal", "WZL"]
+email_list = ["kuberbansal@gmail.com", "info@wzl.io"]
+website_list = ["https://crossculturevc.com", "shipwrecked.vc"]
 subject = "Summer internship"
 
-for i in range(len(data_vc_final)):
-    website = data_vc_final.loc[i]['Website (if available)']
-    email = data_vc_final.loc[i]['Partner Email']
-    name_partner = data_vc_final.loc[i]['Partner Name']
-    
+sender_email = "your_email_adress"
+sender_password = "your_email_password"
+
+#path to pdf
+pdf_file = "C:\\Users\\caneparo\\Desktop\\CV_Paolo_Caneparo.pdf"
+
+for i in range(len(email_list)):
+    investor = investor_name_list[i]
+    email = email_list[i]
+    website = website_list[i]
+
     # Use the OpenAI API to generate the email text
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt=(f"Write an email to {name_partner} asking if they have a summer internship in VC. Personalize the email according to the website. The website is {website}."),
+        prompt=(f"Write an email to {investor} asking if they have a summer internship position in VC open for Paolo Caneparo. Personalize the email according to the website and state that they can know more from my cv and they can contact me when they want for knowing more about me. The website is {website}."),
         temperature=0.5,
         max_tokens=1024
     )
     email_text = response["choices"][0]["text"]
-
+        
     try:
-        server = smtplib.SMTP('mail.privateemail.com', 587)
+        server = smtplib.SMTP("mail.privateemail.com", 587)
         server.starttls()
         server.login(sender_email, sender_password)
 
-         # Create the message
+        # Create the message
         message = MIMEMultipart()
         message["From"] = sender_email
         message["To"] = email
         message["Subject"] = subject
         message.attach(MIMEText(email_text))
-    
+
         # Add the pdf file as an attachment
         with open(pdf_file, "rb") as f:
             attach = MIMEApplication(f.read(), _subtype = "pdf")
             attach.add_header("content-disposition", "attachment", filename = os.path.basename(pdf_file))
             message.attach(attach)
-    
+
         server.sendmail(sender_email, email, message.as_string())
         print(f'Email sent to {email}')
 
@@ -71,3 +70,4 @@ for i in range(len(data_vc_final)):
     finally:
         if server:
             server.quit()
+
