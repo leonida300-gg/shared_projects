616679fbd901807620f577db41c8caf4bf6d513a
Paolo Caneparo <66754232+leonida300-gg@users.noreply.github.com>
2023-01-22 11:23:11 +0100

    Update Send email with chat gpt from excel.py

--- Send email with chat gpt from excel.py
+++ Send email with chat gpt from excel.py
@@ -4,35 +4,31 @@ import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
+import time
+import random
 
 # Set the OpenAI API key
-openai.api_key = "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"
+openai.api_key = "your Open Ai API key"
 
 #import the email, name investor and company website frome excel 
 import pandas as pd
 
-path = 'D:\\Users\\caneparo\\Documents\\VC and startup material\\Ramp_s-VC-_-Angel-Database'
+path = "C:/Users/caneparo/Desktop/Paolo's code projects/trial excel python code.xlsx"
 df = pd.read_excel(path)
 data_vc_final = df[['Website (if available)', 'Partner Name', 'Partner Email']].dropna(axis= 0)
 
 #server definition
 server = None
 
-# Authenticate to the OpenAI API
-#assert "openai" in openai_secret_manager.get_services()
-#secrets = openai_secret_manager.get_secrets("openai")
-#openai.api_key = secrets["api_key"]
-
-sender_email = "paolo@wzl.io"
-sender_password = "PaoloWZL0601"
-pdf_file = "C:\\Users\\caneparo\\Desktop\\New cvs, cover and linkedin\\CV_Paolo_Caneparo"
+sender_email = "your_email_adress"
+sender_password = "your_email_password"
+pdf_file = "C:\\Users\\caneparo\\Desktop\\CV_Paolo_Caneparo.pdf"
 subject = "Summer internship"
 
 for i in range(len(data_vc_final)):
     website = data_vc_final.loc[i]['Website (if available)']
     email = data_vc_final.loc[i]['Partner Email']
     name_partner = data_vc_final.loc[i]['Partner Name']
-    #print("hi Mr." +str(data_vc_final.loc[i]['Partner Name'])+ " I saw your website "+ str(website)+ " and would like to talk to you through " + str(email))
     
     # Use the OpenAI API to generate the email text
     response = openai.Completion.create(
@@ -44,7 +40,7 @@ for i in range(len(data_vc_final)):
     email_text = response["choices"][0]["text"]
 
     try:
-        server = smtplib.SMTP('smtp.open-xchange.com', 535)
+        server = smtplib.SMTP('mail.privateemail.com', 587)
         server.starttls()
         server.login(sender_email, sender_password)
 
@@ -63,6 +59,13 @@ for i in range(len(data_vc_final)):
     
         server.sendmail(sender_email, email, message.as_string())
         print(f'Email sent to {email}')
+
+        #generate a random number between 1 and 10
+        delay = random.randint(1, 10)
+
+        #sleep for the amount of seconds generated
+        time.sleep(delay)
+
     except Exception as e:
         print(f'Error: {e}')
     finally:
